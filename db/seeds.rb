f6e354eeb16a94f802e4a07e4e1a45e03fa230a9
Betty Chang <bwchang@berkeley.edu>
2018-03-14 16:13:32 -0700

    changed name to anova

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.delete_all
-User.create([{ name: 'Zacharias', email: 'demo@demo.com', password: "change_me", remember_created_at: nil }])
+User.create([{ name: 'ANova', email: 'demo@demo.com', password: "change_me", remember_created_at: nil }])
